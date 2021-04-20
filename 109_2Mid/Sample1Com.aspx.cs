e18a54c02d39f0456a52eeefc05d889867c1e751
恕安 賴 <107111212@mail.oit.edu.tw>
2021-04-20 15:37:18 +0800

    1-b

--- 109_2Mid/Sample1Com.aspx.cs
+++ 109_2Mid/Sample1Com.aspx.cs
@@ -8,17 +8,19 @@ using System.Web.UI.WebControls;
 namespace _109_2Mid {
     public partial class Sample1Com : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            lb_Res.Text += Request.QueryString["tb_Acc.text"]+ "<br/>"+ Request.QueryString["tb_Pass"] + "<br/>";
-            if (Request.ServerVariables("cb_Book") == "") {
-                lb_Res.Text += Request.QueryString["cb_Book"];
+            lb_Res.Text += Request.Form.Get("tb_Acc") + "<br/>" + Request.Form.Get("tb_Pass") + "<br/>";
+            Console.WriteLine(Request.Form.Get("cb_Book"));
+
+            if (Request.Form.Get("cb_Book") == "true") {
+                lb_Res.Text +="看書";
             }
-            if (Request.QueryString["cb_Game"] == "")
+            if (Request.Form.Get("cb_Game") == "true")
             {
-                lb_Res.Text += Request.QueryString["cb_Game"];
+                lb_Res.Text += "看書";
             }
-            if (Request.QueryString["cb_Other"] == "")
+            if (Request.Form.Get("cb_Other") == "true")
             {
-                lb_Res.Text += Request.QueryString["cb_Other"];
+                lb_Res.Text += "看書";
             }
 
         }
