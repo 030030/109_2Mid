4e2de1c2d62f0973b03066fa55756dbf10754c00
恕安 賴 <107111212@mail.oit.edu.tw>
2021-04-20 15:11:55 +0800

    1-b

--- 109_2Mid/Sample1Com.aspx.cs
+++ 109_2Mid/Sample1Com.aspx.cs
@@ -8,6 +8,18 @@ using System.Web.UI.WebControls;
 namespace _109_2Mid {
     public partial class Sample1Com : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            lb_Res.Text += Request.QueryString["tb_Acc.text"]+ "<br/>"+ Request.QueryString["tb_Pass"] + "<br/>";
+            if (Request.ServerVariables("cb_Book") == "") {
+                lb_Res.Text += Request.QueryString["cb_Book"];
+            }
+            if (Request.QueryString["cb_Game"] == "")
+            {
+                lb_Res.Text += Request.QueryString["cb_Game"];
+            }
+            if (Request.QueryString["cb_Other"] == "")
+            {
+                lb_Res.Text += Request.QueryString["cb_Other"];
+            }
 
         }
     }
